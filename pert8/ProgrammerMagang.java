c3164b3254171584fab2747b253b99d305955e81
Zaqpurpur-Neo <baristaunescio@gmail.com>
2025-12-07 15:47:10 +0700

    Menamatkan praktikum PBO

--- pert8/ProgrammerMagang.java
+++ pert8/ProgrammerMagang.java
@@ -1,5 +1,5 @@
 // CLASS ProgrammerMagang harus MENGIMPLEMENTASIKAN (implements) KaryawanKontrak dan AksesSistem
-public class ProgrammerMagang /* LENGKAPI implements KaryawanKontrak, AksesSistem */ {
+public class ProgrammerMagang implements KaryawanKontrak, AksesSistem {
     
     // ==========================================================
     // ATRIBUT/STATE (Dibutuhkan untuk menyimpan data objek)
@@ -10,7 +10,10 @@ public class ProgrammerMagang /* LENGKAPI implements KaryawanKontrak, AksesSiste
     // 2. double gajiPerJam
     // 3. String pinRahasia
     // 4. boolean sedangLogin
-    
+    private String nama;
+	private double gajiPerJam;
+	private String pinRahasia;
+	private boolean sedangLogin = false;
     
     // ==========================================================
     // CONSTRUCTOR
@@ -18,7 +21,15 @@ public class ProgrammerMagang /* LENGKAPI implements KaryawanKontrak, AksesSiste
     
     // Buat satu constructor yang menerima 3 parameter (nama, gajiPerJam, pinRahasia).
     // Inisialisasi atribut sedangLogin dengan nilai default 'false'.
-    
+    public ProgrammerMagang(String nama, double gajiPerJam, String pinRahasia) {
+		this.nama = nama;
+		this.gajiPerJam = gajiPerJam;
+		this.pinRahasia = pinRahasia;
+	}
+
+	public String getNama() {
+		return nama;
+	}
     
     // ==========================================================
     // IMPLEMENTASI METHOD DARI KaryawanKontrak
@@ -26,13 +37,27 @@ public class ProgrammerMagang /* LENGKAPI implements KaryawanKontrak, AksesSiste
     
     // @Override: IMPLEMENTASIKAN hitungGaji(int jamKerja)
     // LOGIKA: Hitung gaji (jamKerja * gajiPerJam) dan tampilkan hasilnya.
-    
+   
     // @Override: IMPLEMENTASIKAN perpanjangKontrak(int durasiBulan)
     // LOGIKA: Tampilkan pesan konfirmasi perpanjangan kontrak.
     
     // @Override: IMPLEMENTASIKAN getStatusCuti() (Override Default Method)
     // LOGIKA: Kembalikan nilai String yang spesifik untuk magang: "Tersedia 5 hari".
-    
+   	@Override
+	public double hitungGaji(int jamKerja) {;
+		return (jamKerja * gajiPerJam);
+	}
+
+	@Override
+    public void perpanjangKontrak(int durasiBulan) {
+		System.out.printf("Kontrak diperpanjang %d bulan\n", durasiBulan);
+	}
+
+	@Override
+	public String getStatusCuti() {
+		return "Tersedia 5 hari";
+	}
+
     
     // ==========================================================
     // IMPLEMENTASI METHOD DARI AksesSistem
@@ -48,5 +73,20 @@ public class ProgrammerMagang /* LENGKAPI implements KaryawanKontrak, AksesSiste
     
     // @Override: IMPLEMENTASIKAN getRoleAkses() (Override Default Method)
     // LOGIKA: Kembalikan nilai String yang spesifik untuk magang: "Magang IT".
-    
+   	
+	@Override
+	public void login(String pin) {
+		if(pinRahasia.equals(pin)) {
+			sedangLogin = true;
+			System.out.println("Login Berhasil. Selamat Datang, " + nama);
+		} else {
+			System.out.println("Login Gagal: PIN Salah");
+		}
+	}
+
+	@Override
+	public void logout() {
+		sedangLogin = false;
+		System.out.println("Anda Berhasil Login");
+	}
 }
--- pert8/UjiSDM.java
+++ pert8/UjiSDM.java
@@ -7,22 +7,31 @@ public class UjiSDM {
         
         // 1. BUAT OBJEK ProgrammerMagang
         // Gunakan constructor dengan data: "Andi", 50000.0, "1234".
-        
-        
+		ProgrammerMagang magangNihBoss = new ProgrammerMagang("Andi", 50000, "1234");        
+       	System.out.println("\n=== Pengujian Programmer Magang ===\n"); 
         // 2. PENGUJIAN METODE BERURUTAN
         
         // Panggil method hitungGaji(160).
+		int totalJamGawe = 160;
+		double gaji = magangNihBoss.hitungGaji(totalJamGawe);
+		System.out.printf("Gaji %s (%d) adalah: Rp %f\n", magangNihBoss.getNama(), totalJamGawe, gaji);
         
         // Cetak hasil dari method getStatusCuti().
+		System.out.println("Status Cuti: " + magangNihBoss.getStatusCuti());
         
         // Panggil login("9999") (Tes PIN salah).
-        
+        magangNihBoss.login("9999");
+
         // Panggil login("1234") (Tes PIN benar).
+        magangNihBoss.login("1234");
         
         // Cetak hasil dari method getRoleAkses().
-        
+       System.out.println("Role Akses: " + magangNihBoss.getRoleAkses());
+
         // Panggil perpanjangKontrak(6).
-        
+       magangNihBoss.perpanjangKontrak(6); 
+
         // Panggil logout().
+		magangNihBoss.logout();
     }
 }
